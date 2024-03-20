3b4e5be3f7a39aed6d6f78cc972466da0a81ffd1
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-03-20 22:26:35 +0300

    subtractTwo function

--- functions.dart
+++ functions.dart
@@ -3,7 +3,14 @@ int addTwo(int num1, int num2) {
   return num1 + num2;
 }
 
+// Task 2: Function to subtract two numbers
+double subtractTwo(double num1, double num2) {
+  return num1 - num2;
+}
+
 void main() {
   // Test cases
-  print('The sum is: ${addTwo(5, 3)}'); 
+  print('The sum is: ${addTwo(5, 3)}');
+  print('The Difference is is: ${subtractTwo(5, 3)}');
+
 }
